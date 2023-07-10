526d99e6ec785a0364908c303966008b110fbcb0
Brandon Sauceda <bsauceda@agr.state.ga.us>
2023-07-10 14:11:05 -0400

    completed class 12

--- class12-materials/zbaks/class-main.js
+++ class12-materials/zbaks/class-main.js
@@ -4,10 +4,8 @@ function check() {
   const day = document.querySelector("#day").value;
 
   if (day === "monday" || day === "wednesday" || day === "friday") {
-    alert("today, " + day + ", is a boring day");
+    //how to add
   } else if (day === "tuesday" || day === "thursday") {
-    alert("today, " + day + ", is a class day!");
-  } else if (day === "saturday" || day === "sunday") {
-    alert("it's the weekend!");
-  } else alert("please check input and try again");
+    //how to add
+  } else day = "weekend";
 }
