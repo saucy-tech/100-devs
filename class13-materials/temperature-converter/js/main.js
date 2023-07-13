6e1c10a4334257b48dae630363782407ae53928d
bsauceda@agr.state.ga.us <brandon@saucy.tech>
2023-07-13 14:33:48 -0400

    class 13 live code

--- class13-materials/temperature-converter/js/main.js
+++ class13-materials/temperature-converter/js/main.js
@@ -1,1 +1,13 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+
+//need the value that is in c
+document.querySelector("#convert").addEventListener("click", conversion);
+
+//convert from c to f
+function conversion() {
+  const c = document.querySelector("#c").value;
+  const f = c * 1.8 + 32;
+  document.querySelector("#f").innerText = `${f}`;
+}
+
+//show f
