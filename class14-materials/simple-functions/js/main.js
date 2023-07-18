6aef679f5bc9f55708cadc606a2d75619896ea94
bsauceda@agr.state.ga.us <brandon@saucy.tech>
2023-07-18 12:25:08 -0400

    finish class 14, some hw

--- class14-materials/simple-functions/js/main.js
+++ class14-materials/simple-functions/js/main.js
@@ -1,5 +1,14 @@
 //create a function that adds two numbers and alerts the sum
+function add(num1, num2) {
+  alert(num1 + num2);
+}
 
+add(2, 3);
 //create a function that multiplys three numbers and console logs the product
-
+function multy(num1, num2, num3) {
+  console.log(num1 * num2 * num3);
+}
 //create a function that divides two numbers and returns the ???
+function div(num1, num2) {
+  return num1 / num2;
+}
--- class14-materials/temperature-converter/js/main.js
+++ class14-materials/temperature-converter/js/main.js
@@ -1,1 +1,13 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+
+//Capture input
+document.querySelector("#convert").addEventListener("click", convertor);
+
+//Perform calculation
+function convertor() {
+  const tempC = document.querySelector("#tempC").value;
+  const tempF = tempC * (9 / 5) + 32;
+  document.querySelector("#tempF").innerText = `${tempF}`;
+}
+
+//Show temperature
