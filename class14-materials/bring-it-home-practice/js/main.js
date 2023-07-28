8b584d04615e75f491740ffd674d279f133c815a
bsauceda@agr.state.ga.us <brandon@saucy.tech>
2023-07-28 13:24:25 -0400

    finsihed hw14/15

--- class14-materials/bring-it-home-practice/js/main.js
+++ class14-materials/bring-it-home-practice/js/main.js
@@ -1,39 +1,38 @@
 // *Variables*
 // Create a variable and console log the value
-let firstName = "Brandon";
-console.log(`${firstName}`);
+const gisProgram = "test";
+console.log(gisProgram);
 
 // Create a variable, add 10 to it, and alert the value
-let num = 5;
-num += 10;
-alert(num);
+let myNum = 10;
+alert(num + 10);
 
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
-function subtractFourNumbersAlertDifference(n, n1, n2, n3, n4) {
-  alert(n - (n1 + n2 + n3 + n4));
+function subtractNumbers(n0, n1, n2, n3, n4) {
+  alert(n0 - (n1 + n2 + n3 + n4));
 }
 
 // Create a function that divides one number by another and returns the remainder
-function divideReturnRemainder(n, n1) {
-  return n % n1;
+function findRemainder(n0, n1) {
+  return n0 % n1;
 }
 
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
-function addTwoNumsAlertGreaterThanFifty(n, n1) {
-  if (n + n1 > 50) {
+function checkSum(n0, n1) {
+  if (n0 + n1 > 50) {
     alert("Jumanji");
   } else {
-    alert("NOPE");
+    alert("less than 50");
   }
 }
 
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
-function multiplyThreeNumsCheckDivisibleByThree(n, n1, n2) {
-  if ((n * n1 * n2) % 3 == 0) {
+function checkProduct(n0, n1, n2) {
+  if ((n0 * n1 * n2) % 3 == 0) {
     alert("ZEBRA");
   } else {
-    alert("Nope");
+    alert("not divisiable by 3");
   }
 }
--- class14-materials/class-weekend-boring/js/main.js
+++ class14-materials/class-weekend-boring/js/main.js
@@ -1,17 +1,17 @@
+//Enter day of the week get you have class for tue/thu, boring for mon/wed, and weekend for fri-sun
 //Handle Capital Letters, Place result in DOM, add a check for humpday (Wed)
 
-document.querySelector('#check').addEventListener('click', check)
+//capture input
+document.querySelector("#check").addEventListener("click", dayChecker);
 
-function check() {
-
-  const day = document.querySelector('#day').value
-
-  if(day === "tuesday" || day === "thursday"){
-    console.log("YOU HAVE CLASS")
-  }else if( day === "saturday" || day === "sunday"){
-    console.log("Its The Weekend")
-  }else{
-    console.log("BORING")
+//creat function
+function dayChecker() {
+  const enteredDay = document.querySelector("#day").value.toLowerCase();
+  if (enteredDay == "tuesday" || enteredDay == "thursday") {
+    console.log("class!");
+  } else if (enteredDay == "monday" || enteredDay == "wednesday") {
+    console.log("boring!");
+  } else {
+    console.log("weekend!");
   }
-
 }
