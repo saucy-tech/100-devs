91e740a5bfdfa238b333cd6de4878dc9a915309d
bsauceda@agr.state.ga.us <brandon@saucy.tech>
2023-08-05 14:02:11 -0400

    class 16 exercises

--- class16-materials/age-checker/js/main.js
+++ class16-materials/age-checker/js/main.js
@@ -1,13 +1,52 @@
 //Create a conditonal that checks their age
+let age = 33;
 //If under 16, tell them they can not drive
+if (age < 16) {
+  console.log("you can not drive");
+}
 //If under 18, tell them they can't hate from outside the club, because they can't even get in
+else if (age >= 16 && age < 18) {
+  console.log(
+    "you can't hate from outside the club, because they can't even get in"
+  );
+}
 //If under 21, tell them they can not drink
+else if (age >= 18 && age < 21) {
+  console.log("you can not drink");
+}
 //If under 25, tell them they can not rent cars affordably
+else if (age >= 21 && age < 24) {
+  console.log("you can not rent cars affordably");
+}
 //If under 30, tell them they can not rent fancy cars affordably
+else if (age >= 24 && age < 30) {
+  console.log("you can not rent fancy cars affordably");
+}
 //If under over 30, tell them there is nothing left to look forward too
-
+else {
+  console.log("there is nothing left to look forward too");
+}
 
 //--- Harder
 //On click of the h1
-//Take the value from the input
+document.querySelector("h1").addEventListener("click", ageChecker);
+// //Take the value from the input
+function ageChecker() {
+  let pH = "";
+  let age = document.querySelector("#danceDanceRevolution").value;
+  if (age < 16) {
+    pH = "you can not drive";
+  } else if (age < 18) {
+    pH = "you can't hate from outside the club, because they can't even get in";
+  } else if (age < 21) {
+    pH = "you can not drink";
+  } else if (age < 30) {
+    pH = "you can not rent cars affordably";
+  } else if (age < 35) {
+    pH = "you can not rent fancy cars affordably";
+  } else {
+    pH = "there is nothing left to look forward too";
+  }
+  document.querySelector("p").innerText = pH;
+}
 //Place the result of the conditional in the paragraph
--- class16-materials/simple-functions/js/main.js
+++ class16-materials/simple-functions/js/main.js
@@ -1,12 +1,24 @@
 //---Easy
 //create a function that subtracts two numbers and alerts the difference
-
+function subtractTwo(n0, n1) {
+  alert(n0 - n1);
+}
+subtractTwo(10, 8);
 //create a function that divides three numbers and console logs the quotient
-
+function divideThree(n0, n1, n2) {
+  console.log(n0 / n1 / n2);
+}
+divideThree(15, 5, 2);
 //create a function that multiplys three numbers and returns the product
-
+function multiplyThree(n0, n1, n2) {
+  return n0 * n1 * n2;
+}
+multiplyThree(6, 9, 420);
 //---Medium
 //create a function that takes in three numbers. Add the first two numbers and return the remainder of dividing the sum of the first two numbers by the third number
-
+function addThenModulus(n0, n1, n2) {
+  return (n0 + n1) % n2;
+}
+addThenModulus(17, 5, 4);
 //---Hard
 //create a function that takes in 4 numbers. Multiply the first two numbers. If the product is greater than 100 add the sum of the last two numbers and console log the value. If the product is less that 100, subtract the difference of the last two numbers and console log the value. If the product is 100, multiply the first three numbers together and alert the remainder of dividing the fourth number
