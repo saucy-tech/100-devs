0d34715a943c1cea4549bf8a588997996908822e
Brandon <brandon@saucy.tech>
2023-11-08 06:40:35 -0500

    finish class 19

--- class19-materials/arrays-fight-to-the-finish/js/main.js
+++ class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,7 +1,24 @@
-//Create an array of movie titles. Loop through the array and each element to the h2.
+//Create an array of movie titles. Loop through the array and add each element to the h2.
+let movies = [
+  "The Fellowship of the Ring",
+  "The Two Towers",
+  "The Return of the King",
+  "The Hobbit",
+];
+movies.forEach((x, i) => {
+  document.querySelector("h2").innerText += movies[i];
+});
 
+//Create an array of numbers. Loop through the array and add three to each number and replace the old number.
+let numbers = [10, 20, 30];
+// numbers.forEach((x, i) => {
+//   numbers[i] = x + 3;
+// });
 
-//Create an array of numbers. Loop through the array and three to each number and replace the old number.
-
-
-//Find the average of all the numbers from question three
+//Find the average of all the numbers from the array numbers = [10, 20, 30];
+let total = 0;
+numbers.forEach((x) => {
+  total += x;
+});
+console.log(total);
+console.log(total / numbers.length);
--- class19-materials/arrays-in-it-to-win-it/js/main.js
+++ class19-materials/arrays-in-it-to-win-it/js/main.js
@@ -1,7 +1,11 @@
 //Create an array of movies with at least three movies.
+let favMovies = ["The Lord of the Rings", "Star Wars", "The Three Amigos"];
 
 //Using the array from above, store the first movie in a variable
+let topFavMovie = favMovies[0];
 
 //Get the length of the original array and store it in a new variable
+let movieLength = favMovies.length;
 
 //Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
+let leastFavMovie = favMovies[favMovies.length - 1];
